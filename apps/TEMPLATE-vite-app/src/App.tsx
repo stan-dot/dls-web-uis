1adec0bb5be1a5df498d3f633044eeca5f9c36ba
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-08 08:50:51 +0000

    formatting fixes

--- apps/TEMPLATE-vite-app/src/App.tsx
+++ apps/TEMPLATE-vite-app/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-      <CountButton/>
+        <CountButton />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
