3020ab2642c1b752d312c32fdff80ba767c9f322
Stanislaw Malinowski <stanislaw.malinowski@diamond.ac.uk>
2024-07-08 13:35:29 +0000

    delete not needed vitest

---
+++ vitest.workspace.js
@@ -0,0 +1,7 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./apps/bluegui/vite.config.ts",
+  "./packages/davidia-components/vite.config.ts",
+  "./apps/TEMPLATE-vite-app/vite.config.ts.timestamp-1713430845075-f1acadd409366.mjs"
+])
